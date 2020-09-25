420f20f5f0487a2425b630cb70a9694edd3215a0
miguel <migas.eust@gmail.com>
2020-09-25 15:40:33 -0300

    Implementado menu inicial, funções para guardar dados e lembrar dados

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
-from encrypt import *
+from encrypt import encrypt, decrypt, json
 
 def escrever_json(lista):
     with open('dados/encrypted.json', 'w') as f:
         json.dump(lista, f)
 
-def carregar_json(arquivo):
-    with open(arquivo, 'r') as f:
+def carregar_json():
+    with open('dados/encrypted.json', 'r') as f:
         return json.load(f)
 
+def registrar():
 
-base = carregar_json('dados/base.json')
+    i=0
+    while i < 1:
+        print("para sair igite \"sair\" sem as aspas em qualquer uma das repostas seguintes.\n")
 
-def registrar(endereco, user, senha):
+        endereco = input('\nInsira o site/destino da senha (Ex: email, facebook): ')
+        print('')
+        if endereco == "sair":
+            print("Operação cancelada.\n")
+            return
+        user = input('Usuário associado à senha: ')
+        print('')
+        if user == "sair":
+            print("Operação cancelada.\n")
+            return
+        senha = input('Insira a senha a ser guardada: ')
+        print('')
+        if senha == "sair":
+            print("Operação cancelada.\n")
+            return
+        confirmar_senha = input('Confirme a senha a inserida: ')
+        print('')
+        if senha != confirmar_senha:
+            print('A senha inserida não confere...\nInsira os dados novamente.\n')
+        else:
+            break
 
-    vetor = [user, encrypt(senha)]
 
-    dic = {endereco: vetor}
-
-    escrever_json(dic)
-
+    banco = carregar_json()
+    banco[endereco] = [user] + [encrypt(senha)]
+    escrever_json(banco)
     senha_escondida = ''
     i=0
 
@@ -30,6 +51,28 @@ def registrar(endereco, user, senha):
     print(endereco + '\n' + user + '\n' + senha_escondida)
 
 
+
+
+
+
+def lembrar():
+    endereco = input("Insira o endereço/site do qual você deseja lembrar as informações (Ex: email, facebook) ou \"sair\" sem as aspas para sair. \n")
+    if endereco == 'sair':
+        return
+
+    banco = carregar_json()
+
+    try:
+        dados = banco[endereco]
+
+    except:
+        print("essa entrada não existe")
+
+    print("\n" + endereco + "\n" + dados[0] + "\n" + dados[1] + "\n")
+    x = input("Deseja receber essas informações?(S/N) ")
+    if x == 's' or x == 'S':
+        print("\n" + endereco + "\n" + dados[0] + "\n" + decrypt(dados[1]) + "\n")
+
 if __name__ == "__main__":
 
 #    cripto = encrypt(input("Digite a senha:\n"))
@@ -37,20 +80,28 @@ if __name__ == "__main__":
 #    resposta = input("descriptografar? (S/N)\n")
 #    if resposta == 's':
 #        print(decrypt(cripto))
-    i=0
 
-    while i < 1:
+    print("Bem vindo ao Holocron")
 
-        endereco = input('\nInsira o site/destino da senha: ')
-        print('')
-        user = input('Usuário associado à senha: ')
-        print('')
-        senha = input('Insira a senha a ser guardada: ')
-        print('')
-        confirmar_senha = input('Confirme a senha a inserida: ')
-        print('')
-        if senha != confirmar_senha:
-            print('A senha inserida não confere...\n')
-        else:
-            registrar(endereco, user, senha)
+    while True:
+
+        resposta = input("para guardar um cadastro digite 1, para lembrar um digite 2, para sair digite 0.\n")
+
+        if resposta == '1':
+            registrar()
+            x = input("desja realizar outra operação? (S/N) ")
+            if x == "n" or x == "N":
+                break
+        elif resposta == '2':
+            lembrar()
+            x = input("desja realizar outra operação? (S/N) ")
+            if x == "n" or x == "N":
+                break
+        elif resposta == '0':
             break
+        else:
+            print("Erro, símbolo inserido não reconhecido")
+            x = input("desja realizar outra operação? (S/N) ")
+            if x == "n" or x == "N":
+                break
+        
